ebef7a00a0d153fb13c2e55173f0ccf2fa4ceb76
Victor Rivera <verm2405@gmail.com>
2019-05-11 15:16:20 -0400

    BT-001 finish initial nav bar
    • Makes nav bar responsive
    • Moves app css to app css file
    
    • TODO:
    - Add search bar to nav
    - Make it toggeable menu when on phone devices.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,8 +4,15 @@ import "../styles/NavBar.css";
 const NavBar = () => {
   return (
     <div className="nav-bar">
-      <h1>Butterd</h1>
-      <button>Sign Up</button>
+      <h3>Butterd</h3>
+      <div className="nav-bar-links">
+        <p>Movies</p>
+        <p>News</p>
+      </div>
+      <div className="nav-bar-account">
+        <p>Create Account</p>
+        <button>Sign In</button>
+      </div>
     </div>
   );
 };
